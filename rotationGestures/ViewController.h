c8795a633598c47c6665eb91b34eab894da13256
student <student@achiever-6.local>
2017-09-02 11:34:20 +0530

    Initial Commit

---
+++ rotationGestures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rotationGestures
+//
+//  Created by student on 9/2/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ rotationGestures/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  rotationGestures
+//
+//  Created by student on 9/2/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
